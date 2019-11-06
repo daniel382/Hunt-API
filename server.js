500ed701c5299830625d64d23c16b26acd85b047
Daniel Lucas <daniel.camargo@dokoro.com.br>
2019-11-06 18:43:11 -0300

    Adicionando Cors

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
 const requireDir = require('require-dir')
+const cors = require('cors')
 
 const host = '127.0.0.1'
 const port = 3000
@@ -10,6 +11,7 @@ const port = 3000
 mongoose.connect(`mongodb://${host}:27017/nodeapi`, { useNewUrlParser: true, useUnifiedTopology: true })
 requireDir('./src/models')
 
+app.use(cors())
 app.use(express.json())
 app.use('/api', require('./src/routes'))
 
